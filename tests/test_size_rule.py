20197b1f14f0f62f7877cfc926ef003da85e1c45
beanteacher <ohmk98@gmail.com>
2026-02-27 15:59:02 +0900

    test(ai): day9 strengthen reason rule tests

--- tests/test_size_rule.py
+++ tests/test_size_rule.py
@@ -5,6 +5,7 @@ def test_predict_size_m_profile():
     size, reasons = predict_size(170, 68, 'male')
     assert size == 'M'
     assert 'height_under_175' in reasons
+    assert 'weight_under_75' in reasons
 
 
 def test_predict_size_l_by_height():
@@ -17,3 +18,4 @@ def test_predict_size_l_by_weight():
     size, reasons = predict_size(170, 80, 'female')
     assert size == 'L'
     assert 'weight_over_or_eq_75' in reasons
+    assert 'non_male_profile_baseline' in reasons
